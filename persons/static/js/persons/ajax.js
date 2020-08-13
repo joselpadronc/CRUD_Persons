ff5362e30dca17ea0f4a875c7106a3d499cc0464
José Padrón <joselpadronc@localhost.localdomain>
2020-08-13 02:06:13 -0400

    Adding code at ajax.js File

---
+++ persons/static/js/persons/ajax.js
@@ -0,0 +1,37 @@
+(()=> {
+    const xhr = new XMLHttpRequest(),
+        $xhr = document.getElementById('xhr'),
+        $fragment = document.createDocumentFragment();
+
+    xhr.addEventListener('readystatechange', e => {
+        if (xhr.readyState !== 4) {
+            return;
+        }
+
+        if (xhr.status >= 200 && xhr.status < 300) {
+            // console.log(xhr.responseText);
+            console.log("Exito");
+
+            console.log(xhr.responseText);
+            //let json = JSON.parse(xhr.responseText);
+            //console.log(json);
+
+            //json.forEach((el) => {
+            //    const $li = document.createElement('li');
+            //    $li.innerHTML = `${el.name} --- ${el.email} --- ${el.phone}`;
+            //    $fragment.appendChild($li);
+            //});
+
+            //$xhr.appendChild($fragment);
+
+        }else {
+            console.log("Error");
+            let message = xhr.statusText || 'Ocurrio un error :(';
+            $xhr.innerHTML = `Error ${xhr.status}:${message}`;
+        }
+    });
+
+    xhr.open('GET', "/list-persons/");
+
+    xhr.send()
+})(); 
