00b8782dfc7f68d7825c98aba077a0a4c3c5a1a2
José Padrón <joselpadronc@localhost.localdomain>
2020-08-14 11:09:48 -0400

    Update codo of ajax

--- persons/static/js/persons/ajax.js
+++ persons/static/js/persons/ajax.js
@@ -1,12 +1,13 @@
 function ajax () {
-    const request = new XMLHttpRequest()
-    const url = '/list-persons/'
+    const request = new XMLHttpRequest();
+    const url = '/list-persons/';
 
-    request.onreadystatechange = () => {
+    request.addEventListener('readystatechange', e => {
         if (this.readyState == 4 && this.readyStatus == 200) {
-            console.log(this.responseText);
+            let jsonResponse = JSON.parse(this.responseText);
+            console.log(responseText);
         }
-    }
+    });
 
     request.open('GET', url);
     request.send();
