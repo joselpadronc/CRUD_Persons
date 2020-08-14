0bdf269cab4ecd28eba819f0481a65a70269cbe9
José Padrón <joselpadronc@localhost.localdomain>
2020-08-14 10:47:35 -0400

    New code of ajax for updating with button

--- persons/static/js/persons/ajax.js
+++ persons/static/js/persons/ajax.js
@@ -1,36 +1,17 @@
-(()=> {
-    const xhr = new XMLHttpRequest(),
-        $xhr = document.getElementById('xhr'),
-        $fragment = document.createDocumentFragment();
+function ajax () {
+    const request = new XMLHttpRequest()
+    const url = '/list-persons/'
 
-    xhr.addEventListener('readystatechange', e => {
-        if (xhr.readyState !== 4) {
-            return;
+    request.onreadystatechange = () => {
+        if (this.readyState == 4 && this.readyStatus == 200) {
+            console.log(this.responseText);
         }
+    }
 
-        if (xhr.status >= 200 && xhr.status < 300) {
-            // console.log(xhr.responseText);
-            console.log("Exito");
+    request.open('GET', url);
+    request.send();
+}
 
-            let json = JSON.parse(xhr.responseText);
-            console.log(json);
-
-            //json.forEach((el) => {
-            //    const $li = document.createElement('li');
-            //    $li.innerHTML = `${el.name} --- ${el.email} --- ${el.phone}`;
-            //    $fragment.appendChild($li);
-            //});
-
-            //$xhr.appendChild($fragment);
-
-        }else {
-            console.log("Error");
-            let message = xhr.statusText || 'Ocurrio un error :(';
-            $xhr.innerHTML = `Error ${xhr.status}:${message}`;
-        }
-    });
-
-    xhr.open('GET', "/list-persons/");
-
-    xhr.send()
-})(); 
+document.getElementById('update-list-btn').addEventListener('click', () => {
+    ajax();
+});
