efce0fbce4af956fc39dd2b6ef668803ab1373f9
José Padrón <padroncjosel@gmail.com>
2020-08-21 09:47:23 -0400

    Adding new code for method GET with AJAX

--- persons/static/js/persons/ajaxGet.js
+++ persons/static/js/persons/ajaxGet.js
@@ -1,54 +1,62 @@
-function ajax () {
-    const httpRequest = new XMLHttpRequest()
-    const url = '/list-persons/'
+const request = ((method, url, cb) => {
+    const xhr = new XMLHttpRequest()
+    const prefix = 'http://localhost:8000';
 
-    httpRequest.addEventListener('readystatechange', e => {
+    xhr.addEventListener('readystatechange', e => {
 
-        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
-            let data = JSON.parse(httpRequest.responseText);
-
-            let res = document.getElementById('res');
-            res.innerHTML = '';
-
-            for (let person of data) {
-
-                res.innerHTML += `
-                    <tr>
-                        <td>
-                            ${person.name}
-                        </td>
-                        <td>
-                            ${person.id_card}
-                        </td>
-                        <td>
-                            ${person.nationality}
-                        </td>
-                        <td>
-                            ${person.age}
-                        </td>
-                        <td>
-                            <a href="/editar/${person.id}/">
-                                <button type="button" class="btn btn-warning">Editar</button>
-                            </a>
-                            <a href="#" id="delete-btn">
-                                <button type="button" class="btn btn-danger">Eliminar</button>
-                            </a>
-                        </td>
-                    </tr>
-                `;
-            }
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            return cb ? cb(JSON.parse(xhr.responseText)) : null;
+        
+        } else {
+            return new Error('Network Error');
         }
     });
 
-    httpRequest.open('GET', url);
+    
+    xhr.open(method, prefix + url);
 
-    httpRequest.send()
+    xhr.send()
+});
+
+
+const renderRows = ((pyload) => {
+    const table = document.querySelector('#table');
+    const tbody = table.querySelector('#tbody');
+    tbody.innerHTML = '';
+
+    for (let person of pyload) {
+        tbody.innerHTML += `
+            <tr>
+                <td>
+                    ${person.name}
+                </td>
+                <td>
+                    ${person.id_card}
+                </td>
+                <td>
+                    ${person.nationality}
+                </td>
+                <td>
+                    ${person.age}
+                </td>
+                <td>
+                    <a href="/editar/${person.id}/">
+                        <button type="button" class="btn btn-warning">Editar</button>
+                    </a>
+                    <a href="#" id="delete-btn">
+                        <button type="button" class="btn btn-danger">Eliminar</button>
+                    </a>
+                </td>
+            </tr>
+        `;
+    }
+});
+
+window.onload = () => {
+    const reloadBtn = document.getElementById('update-list-btn');
+    request("GET", /list-persons/, renderRows);
+
+    reloadBtn.addEventListener('click', () => {
+        request("GET", /list-persons/, renderRows);
+    });
 }
-
-ajax();
-
-document.getElementById('update-list-btn').addEventListener('click', () => {
-    ajax();
-}); 
-
-
